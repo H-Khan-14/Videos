e3d50d7be4baf464e159dd0f656844a4ef734abd
H-Khan-14 <haseebkhan14@hotmail.com>
2021-09-07 16:55:49 -0400

    Updates

---
+++ precache-manifest.bff4ab66c131fbf68d80acf8d1574ba5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f820c620a7dc69d5e98123964f95ee",
+    "url": "/Videos/index.html"
+  },
+  {
+    "revision": "429680b433645b58180d",
+    "url": "/Videos/static/css/main.4a966e5b.chunk.css"
+  },
+  {
+    "revision": "ff1e8fd24fc739ff85a8",
+    "url": "/Videos/static/js/2.85523917.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/Videos/static/js/2.85523917.chunk.js.LICENSE"
+  },
+  {
+    "revision": "429680b433645b58180d",
+    "url": "/Videos/static/js/main.4e3aaa00.chunk.js"
+  },
+  {
+    "revision": "b2bf0ce44d6ffb9a495a",
+    "url": "/Videos/static/js/runtime-main.e608b381.js"
+  }
+]);
